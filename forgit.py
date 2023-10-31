283e1e7c5e3ecdd280ac889d5ea24eeb33a22567
vighnesh <vignesh.kumar@rogvic.com>
2023-10-31 13:18:59 +0530

    Update the code

--- forgit.py
+++ forgit.py
@@ -1,1 +1,5 @@
 print("This file is added after the pushing of previous files into git repositories")
+
+
+
+print("I have added this code for modification in git")
--- forgit2.py
+++ forgit2.py
@@ -1,1 +1,2 @@
 print("This is for another push for git")
+print("This is also modified")
